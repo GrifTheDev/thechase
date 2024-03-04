706f92e50d3ac78590a5cb12dc318f4675dfc5f5
GrifTheDev <70777013+GrifTheDev@users.noreply.github.com>
2024-03-04 21:35:26 +0100

    no time but yeah did something

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,16 @@
 import { getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { FB_API_KEY, FB_AUTH_DOMAIN, FB_PROJECT_ID, FB_STORAGE_BUCKET, FB_MESSAGING_SENDER_ID, FB_APP_ID, FB_MEASUREMENT_ID } from "$env/static/private"
+import { PUBLIC_FB_API_KEY, PUBLIC_FB_AUTH_DOMAIN, PUBLIC_FB_PROJECT_ID, PUBLIC_FB_STORAGE_BUCKET, PUBLIC_FB_MESSAGING_SENDER_ID, PUBLIC_FB_APP_ID, PUBLIC_FB_MEASUREMENT_ID } from "$env/static/public"
 export function getDB() {
   if (getApps().length == 0) {
     const firebaseCfg = {
-      apiKey: FB_API_KEY,
-      authDomain: FB_AUTH_DOMAIN,
-      projectId: FB_PROJECT_ID,
-      storageBucket: FB_STORAGE_BUCKET,
-      messagingSenderId: FB_MESSAGING_SENDER_ID,
-      appId: FB_APP_ID,
-      measurementId: FB_MEASUREMENT_ID,
+      apiKey: PUBLIC_FB_API_KEY,
+      authDomain: PUBLIC_FB_AUTH_DOMAIN,
+      projectId: PUBLIC_FB_PROJECT_ID,
+      storageBucket: PUBLIC_FB_STORAGE_BUCKET,
+      messagingSenderId: PUBLIC_FB_MESSAGING_SENDER_ID,
+      appId: PUBLIC_FB_APP_ID,
+      measurementId: PUBLIC_FB_MEASUREMENT_ID,
     };
 
     initializeApp(firebaseCfg);
